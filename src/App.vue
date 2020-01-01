df612cc5b360b159abe157d946c5f8134d59ac03
ibrahimd1 <idarakcilar@gmail.com>
2020-01-01 20:05:01 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="activeComponent"></component>
+</template>
+
+<script>
+import GameCards from "./components/GameCards";
+import Celebrate from "./components/Celebrate";
+import Failure from "./components/Failure";
+
+export default {
+  components: {
+    appGameCards: GameCards,
+    appCelebrate: Celebrate,
+    appFailure: Failure
+  },
+  data() {
+    return {
+      activeComponent: "app-game-cards"
+    };
+  }
+};
+</script>
+<style>
+body {
+  font-family: sans-serif;
+}
+</style>
---
+++ src/components/GameCards.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="game-area">
+    <h1 class="title">
+      Poğaça
+      <span>Nerede</span>
+      <strong>?</strong>
+    </h1>
+    <h4 class="description">Açık kartlardan birini seçtikten sonra,kapalı olan karta tıklayınız</h4>
+    <div class="container">
+      <app-card
+        @click.native="selectedCard=card.id"
+        v-for="card in cards"
+        :key="card.id"
+        :card="card"
+        :class="{'shadow':selectedCard==card.id}"
+      ></app-card>
+    </div>
+    <div class="container">
+      <app-default-card></app-default-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "./Card";
+import DefaultCard from "./DefaultCard";
+export default {
+  components: {
+    appCard: Card,
+    appDefaultCard: DefaultCard
+  },
+  data() {
+    return {
+      selectedCard: null,
+      cards: [
+        { id: 1, component: "app-cards", image: "/src/assets/card-1.jpg" },
+        { id: 2, component: "app-cards", image: "/src/assets/card-2.jpg" },
+        { id: 3, component: "app-cards", image: "/src/assets/card-3.jpg" },
+        { id: 4, component: "app-cards", image: "/src/assets/card-4.jpg" },
+        { id: 5, component: "app-cards", image: "/src/assets/card-5.jpg" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.title {
+  text-align: center;
+  color: rosybrown;
+}
+
+.title span {
+  color: mediumpurple;
+}
+
+.title strong {
+  color: darkred;
+}
+
+.description {
+  color: grey;
+  text-align: center;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+}
+
+.shadow {
+  box-shadow: 0px 5px 48px #30969f !important;
+  transition: box-shadow 0.5s;
+}
+</style>
