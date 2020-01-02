cc867ea2ecfd1b6d392b0fe3ccbf1b4c86d1547e
ibrahimd1 <idarakcilar@gmail.com>
2020-01-02 23:02:02 +0300

    game scene

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <component @isCorrectEvent="activeComponent=$event" :is="activeComponent"></component>
+  <transition-group name="slideContainer" mode="out-in">
+    <component key="mainComponent" @isCorrectEvent="activeComponent=$event" :is="activeComponent"></component>
+    <canvas key="canvas" id="canvas" v-show="activeComponent == 'app-celebrate'"></canvas>
+  </transition-group>
 </template>
 
 <script>
@@ -23,5 +26,38 @@ export default {
 <style>
 body {
   font-family: sans-serif;
+}
+
+.slideContainer-enter {
+}
+.slideContainer-enter-active {
+  animation: slide-in 0.3s ease-out forwards;
+}
+.slideContainer-leave {
+}
+.slideContainer-leave-active {
+  animation: slide-out 0.3s ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(-1000px);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0px);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateX(0px);
+    opacity: 1;
+  }
+  to {
+    transform: translateX(1000px);
+    opacity: 0;
+  }
 }
 </style>
